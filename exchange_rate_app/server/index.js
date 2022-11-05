246cda8c5733e4e9f7fa191393ce9080f1553450
Mdshahbazalam6 <shahbazalam2002620@gmail.com>
2022-11-05 18:03:04 +0530

    created the backend and implemented Local nodejs server to be used to prefetch the required data from the open public api

---
+++ exchange_rate_app/server/index.js
@@ -0,0 +1,52 @@
+import express from 'express'
+import cors from "cors"
+const app = express()
+import fetch from 'node-fetch';
+import bodyparser from 'body-parser'
+
+app.use(cors())
+
+app.get('/',(req,res)=>{
+
+var requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+  headers: {
+    "apikey" :  "bKTS3F3e7OWfxfKs0g6iOA6RXYSWDuEe"
+  }
+};
+
+fetch("https://api.apilayer.com/exchangerates_data/latest?symbols=&base=EUR", requestOptions)
+  .then(response => response.text())
+  .then(result => res.status(200).send(result))
+  .catch(error => console.log('error', error));      
+})
+
+var cache = {};
+
+app.get('/getrates', bodyparser.json(),function(req, res, next) {
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow',
+        headers: {
+          "apikey" :  "bKTS3F3e7OWfxfKs0g6iOA6RXYSWDuEe"
+        }
+    };
+  if (cache.rates) {
+    res.send(cache.rates);
+  } else {
+    fetch("https://api.apilayer.com/exchangerates_data/latest?symbols=&base=INR", requestOptions)
+    .then(response => response.json()) 
+    .then(response => {
+      cache.rates = response.rates;
+      console.log(cache)
+      res.send(response.rates);
+    })
+  }
+});
+
+setInterval(function() {
+  delete cache.rates;
+}, 60 * 1000)
+
+app.listen(5000)
